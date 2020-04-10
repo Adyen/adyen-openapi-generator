b98b7964aa6a6b4281dc6d3f3c285042b31d1bae
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-04-10 10:58:58 +0200

    new generator

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const json = require("./config.json");
 
 const tmpFolder = path.resolve(__dirname, "./tmp");
 const baseUrl = "https://docs.adyen.com/api-explorer/json";
+const createHeaderTxt = (service, version) => `
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen NodeJS API Library
+ *
+ * Version of ${service}: v${version}
+ *
+ * Copyright (c) 2019 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+ 
+`
 
 shell.mkdir(tmpFolder);
 shell.exec(`wget ${baseUrl}/Services.json -O ${tmpFolder}/services.json -q`);
@@ -18,18 +43,25 @@ Object.entries(services).forEach(([name, service], idx) => {
     const serviceFolder = path.join(tmpFolder, formattedName);
     shell.mkdir(serviceFolder);
     const versions = Object.entries(service.versions);
-    shell.echo(`   ┌─ ${name}  (${idx + 1}/${Object.entries(services).length})`);
-    versions.forEach(([key, value], index) => {
-        const versionFolder = path.join(serviceFolder, `v${key}`);
-        const filePath = `${versionFolder}/${formattedName}.json`;
-        shell.mkdir(versionFolder);
-        shell.echo(`   │   ┌  v${key} (${index + 1}/${versions.length})`); 
-        shell.echo(`   │   │ 🗜   Downloading OpenAPI file from  ${baseUrl}/${value.reference}`);
-        shell.exec(`wget ${baseUrl}/${value.reference} -O ${filePath} -q`);
-        shell.echo(`   │   │ ⚙️   Generating specs for ${name} v${key}`);
-        shell.exec(`npm run build -- generate -i ${filePath} -g ${json.client} -o ${path.join(__dirname, json.outputPath, formattedName, `v${key}`)} -Dmodels >/dev/null`);
-        shell.echo("   │   └ ✔️  done");
-    });
+    shell.echo(`   ┌─ ${name}`);
+    const [key, value] = versions.pop();
+    const versionFolder = path.join(serviceFolder, `v${key}`);
+    const filePath = `${versionFolder}/${formattedName}.json`;
+    const outputPath = path.join(__dirname, json.outputPath, `${formattedName}.ts`);
+    const headerTxt = createHeaderTxt(name, key);
+
+    shell.mkdir(versionFolder);
+    shell.echo(`   │   ┌  Latest version: ${key}`);
+    shell.echo(`   │   │ 🗜   Downloading OpenAPI file from  ${baseUrl}/${value.reference}`);
+    shell.exec(`wget ${baseUrl}/${value.reference} -O ${filePath} -q`);
+    shell.echo(`   │   │ ⚙️   Generating specs for ${name} v${key}`);
+    shell.exec(`./node_modules/dtsgenerator/bin/dtsgen -n I${name.replace(/\s/g, "")} -o ${outputPath} ${filePath}`);
+    const data = fs.readFileSync(outputPath).toString().split("\n");
+    data.splice(0, 0, headerTxt);
+    const text = data.join("\n");
+    fs.writeFile(outputPath, text, e => { shell.exit(e) });
+    // shell.exec(`npm run build -- generate -i ${filePath} -g ${json.client} -o ${path.join(__dirname, json.outputPath, formattedName, `v${key}`)} -DnpmName=@adyen/api-library -DsupportsES6=true -Dmodels >/dev/null`);
+    shell.echo("   │   └ ✔️  done");
     shell.echo(`   └─ Done!`);
 });
 
